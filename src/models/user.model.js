55074c516f67f2e7580b26d4277dc44138408022
bappi12312 <ranamdbappihossain@gmail.com>
2024-11-20 18:46:50 +0600

    user model completed

---
+++ src/models/user.model.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    index: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    index: true,
+    lowerCase: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['buyer', 'seller', 'admin'],
+    default: 'buyer'
+  },
+  wishList: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product'
+    }
+  ],
+  cart: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Cart'
+    }
+  ]
+}, { timestamps: true })
+
+
+userSchema.pre("save",async function(next) {
+  if(!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password,10);
+  next()
+})
+
+userSchema.methods.generateAccessToken = function() {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name
+    },
+    process.env.ACCESS_TOKEN_SECRET ,
+    {
+      expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+      {
+          _id: this._id,
+      },
+      process.env.REFRESH_TOKEN_SECRET,
+      {
+          expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+      }
+  )
+}
+
+export const User = mongoose.model("User",userSchema)
