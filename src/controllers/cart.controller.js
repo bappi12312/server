6bcfa4ca1c25b034d1a59ff0d8bc3c7b26435350
bappi12312 <ranamdbappihossain@gmail.com>
2024-11-21 10:58:46 +0600

    removeProcut controllers added

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -8,26 +8,26 @@ import { isValidObjectId } from "mongoose";
 import { Cart } from "../models/cart.model.js";
 
 // get cart by specific user
-const getCart = asyncHandler(async(req,res) => {
+const getCart = asyncHandler(async (req, res) => {
   try {
-    const cart = await Cart.findOne({user: req.user?._id}).populate("items.product")
+    const cart = await Cart.findOne({ user: req.user?._id }).populate("items.product")
 
     if (!cart) return res.json({ items: [] });
 
     return res.status(200).json(
-      new ApiResponse(200,cart,"get products cart")
+      new ApiResponse(200, cart, "get products cart")
     )
   } catch (error) {
-    
+
   }
 })
 
 // add to cart controller by a seller
-const addToCart = asyncHandler(async(req,res) => {
-  const {porductId,quantity} = req.body;
+const addToCart = asyncHandler(async (req, res) => {
+  const { porductId, quantity } = req.body;
 
-  if(!isValidObjectId(porductId)) {
-    throw new ApiError(400,"invalid productID")
+  if (!isValidObjectId(porductId)) {
+    throw new ApiError(400, "invalid productID")
   }
 
   try {
@@ -35,7 +35,7 @@ const addToCart = asyncHandler(async(req,res) => {
       user: req.user?._id
     })
 
-    if(!cart) {
+    if (!cart) {
       cart = new Cart({
         user: req.user?._id,
         items: []
@@ -44,23 +44,52 @@ const addToCart = asyncHandler(async(req,res) => {
 
     const itemIndex = cart.items?.findIndex(item => item.product.toString() === porductId)
 
-    if(itemIndex > -1) {
+    if (itemIndex > -1) {
       cart.items[itemIndex].quantity += quantity;
     } else {
-      cart.items.push({product: porductId,quantity: 1})
+      cart.items.push({ product: porductId, quantity: 1 })
     }
 
     await cart.save()
 
     return res.status(200).json(
-      new ApiResponse(200,cart?.items,"product added to cart successfully")
+      new ApiResponse(200, cart?.items, "product added to cart successfully")
     )
   } catch (error) {
-    throw new ApiError(500,error?.message)
+    throw new ApiError(500, error?.message)
+  }
+})
+
+// remove product from cart
+const removeProduct = asyncHandler(async (req, res) => {
+  const { porductId } = req.body;
+
+  try {
+    const cart = await Cart.findOne({ user: req.user?._id })
+    if (!cart) {
+      throw new ApiError(400, "cart not found")
+    }
+
+    const itemIndex = cart.items.findIndex((item) => item.product.toString() === porductId)
+
+    if (itemIndex === -1) {
+      throw new ApiError(404, "product not found in cart")
+    }
+
+    cart.items.splice(itemIndex, 1)
+    await cart.save()
+
+    return res.status(200).json(
+      new ApiResponse(200, cart?.items, "product remove from cart successfully")
+    )
+  } catch (error) {
+    console.error('Error removing from cart:', error);
+    res.status(500).json({ success: false, message: 'Internal server error' });
   }
 })
 
 export {
   getCart,
   addToCart,
+  removeProduct
 }
